812265b8f443b5031a193bbadb7495084bd49b81
Abdullah Masood <abdullahmasoodx@gmail.com>
2025-12-23 02:47:31 +0500

    creating Register page

---
+++ src/views/auth/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className='min-w-screen min-h-screen flex bg-[#38BAF0] items-center justify-center'>
+            <div className='w-[350px] text-[#ffffff] p-2'>
+                <div className='bg-[#6f68d1] p-4 rounded-md'>
+                    <h2 className='text-xl mb-3 font-bold'>
+                     Welcome To Ecommerce
+                    </h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
